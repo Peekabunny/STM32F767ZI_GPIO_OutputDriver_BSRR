355f6c7c92c413bdae7d6ffde21d5f7c65a971cb
Vincent Do <vinvdo123@gmail.com>
2026-05-12 21:26:42 -0400

    Add STM32F767ZI GPIO output driver - LED blink

---
+++ Src/gpio.c
@@ -0,0 +1,29 @@
+#include "gpio.h"
+
+#define GPIOBEN (1U << 1)
+
+// bit set and bit reset at pin 0 for led
+#define LED_BS0 (1U << 0) //Bit set pin 0
+#define LED_BR0 (1U << 16)//Bit reset pin 0
+
+
+
+void led_init(void)
+{
+    /*Enable clock access to GPIOB*/
+    RCC->AHB1ENR |= GPIOBEN;
+    /*Set PA5 mode to output mode*/
+    GPIOB->MODER |= (1U<<0);  //set
+    GPIOB->MODER &= ~(1U<<1); // clear
+}
+
+void led_on(void)
+{
+    /*Set PA5 high*/
+    GPIOB->BSRR |=LED_BS0;
+}
+void led_off(void)
+{
+    /*Set PA5 low*/
+    GPIOB->BSRR |=LED_BR0;
+}
---
+++ Src/gpio.h
@@ -0,0 +1,13 @@
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "stm32f7xx.h"
+
+void led_init(void);
+
+void led_on(void);
+
+void led_off(void);
+
+#endif /* GPIO_H_ */
